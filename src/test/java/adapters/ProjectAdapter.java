79655b4b9e5cfa5663d32ac1ffaf119a4e79013d
vikakazak <viktoria.kazak0@gmail.com>
2023-06-14 16:50:03 +0300

    Added logs

--- src/test/java/adapters/ProjectAdapter.java
+++ src/test/java/adapters/ProjectAdapter.java
@@ -1,13 +1,16 @@
 package adapters;
 
 import dto.Project;
+import lombok.extern.log4j.Log4j2;
 
 import static io.restassured.RestAssured.*;
 
+@Log4j2
 public class ProjectAdapter extends BaseAdapter{
     public static final String POSTFIX = "/v1/project/";
 
     public void create(Project project) {
+        log.info("Create project by api: {}", project);
         requestSpecification.
                 body(project).
                 log().body().
--- src/test/java/dto/ProjectFactory.java
+++ src/test/java/dto/ProjectFactory.java
@@ -1,11 +1,14 @@
 package dto;
 
 import com.github.javafaker.Faker;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class ProjectFactory {
 
     public static Project getRandom() {
         Faker faker = new Faker();
+        log.info("Create project by project factory");
         return Project.builder()
                 .title(faker.name().firstName())
                 .code(faker.name().firstName().toUpperCase())
