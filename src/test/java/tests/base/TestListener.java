c53d9b73f0e70553dd54cc4c131caa35f7efe113
vikakazak <viktoria.kazak0@gmail.com>
2023-06-14 16:41:59 +0300

    Fixed TestListener

--- src/test/java/tests/base/TestListener.java
+++ src/test/java/tests/base/TestListener.java
@@ -18,13 +18,13 @@ public class TestListener implements ITestListener {
 
     @Override
     public void onTestSuccess(ITestResult iTestResult) {
-        log.info(String.format("======================================== FINISHED TEST {} Duration: {}s ========================================", iTestResult.getName(),
+        log.info(String.format("======================================== FINISHED TEST %s Duration: %ss ========================================", iTestResult.getName(),
                 getExecutionTime(iTestResult)));
     }
 
     @Override
     public void onTestFailure(ITestResult iTestResult) {
-        log.info(String.format("======================================== FAILED TEST {} Duration: {}s ========================================", iTestResult.getName(),
+        log.info(String.format("======================================== FAILED TEST %s Duration: %ss ========================================", iTestResult.getName(),
                 getExecutionTime(iTestResult)));
         WebDriver driver = (WebDriver) iTestResult.getTestContext().getAttribute("driver");
         AllureUtils.takeScreenshot(driver);
@@ -32,7 +32,7 @@ public class TestListener implements ITestListener {
 
     @Override
     public void onTestSkipped(ITestResult iTestResult) {
-        log.info(String.format("======================================== SKIPPING TEST {} ========================================", iTestResult.getName()));
+        log.info(String.format("======================================== SKIPPING TEST %s ========================================", iTestResult.getName()));
     }
 
     @Override
